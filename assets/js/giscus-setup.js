b283a0b32f219902fa27e861ca997429cb0c542c
davidwangprc <99197078+davidwangprc@users.noreply.github.com>
2025-11-22 03:38:36 +0000

    Deploying to gh-pages from @ davidwangprc/davidwangprc.github.io@6ac489b4404c3eecf40a53197796d320d885f11e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "davidwangprc/davidwangprc.github.io-blog",
+    "data-repo": "davidwangprc/davidwangprc.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
